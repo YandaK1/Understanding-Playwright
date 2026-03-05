60bd47aad56ef7e570df0ed6498c02cc766de068
YandaK1 <yanda.kiviet@redacademy.co.za>
2026-03-05 08:21:25 +0200

    user can login to dashboard test

---
+++ tests/prac2.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('user can log into dashboard', async ({ page }) => {
+  await page.goto('https://quality-engineering-labs.vercel.app/login.html');
+  await page.fill('#login-username', 'admin');
+  await page.fill('#login-password', 'password123');
+  await page.click('button[type="submit"]');
+//   await expect(page).toHaveText(/Welcome back, Admin/i);
+await expect(page.getByTestId('dashboard-title')).toHaveText('Welcome back, Admin');
+});
--- tests/yanda-prac.spec.ts
+++ tests/yanda-prac.spec.ts
@@ -1,10 +1,10 @@
 //here Yanda is trying to learn the syntax of a playwright test, everything from the brackets all the way down to the last comma
 
-import {test, expect} from '@playwright/test'
+// import {test, expect} from '@playwright/test'
   
-test('test home page', async ({page})=>{
-    await page.goto('https://quality-engineering-labs.vercel.app/index.html');
-    await page.getByRole('link', { name: 'Home' }).click();
-    await expect(page).toHaveTitle(/MerchantHub/i);
-});
+// test('test home page', async ({page})=>{
+//     await page.goto('https://quality-engineering-labs.vercel.app/index.html');
+//     await page.getByRole('link', { name: 'Home' }).click();
+//     await expect(page).toHaveTitle(/MerchantHub/i);
+// // });
   
